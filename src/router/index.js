19c3c6a2a0487e3f3104cb9616ace295092f91af
hobin49 <hobin505@gmail.com>
2023-02-23 16:44:55 +0900

    fix : entrance에 있던 vue파일이랑 연결된 라우터 삭제 [이호빈]

--- src/router/index.js
+++ src/router/index.js
@@ -17,30 +17,6 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-    path: "/entrance/first",
-    name: "first",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Entrance/FirstPage.vue"),
-  },
-  {
-    path: "/entrance/second",
-    name: "second",
-    component: () => import("../views/Entrance/SecondPage.vue"),
-  },
-  {
-    path: "/entrance/third",
-    name: "third",
-    component: () => import("../views/Entrance/ThirdPage.vue"),
-  },
-  {
-    path: "/entrance/fourth",
-    name: "fourth",
-    component: () => import("../views/Entrance/FourthPage.vue"),
-  },
-  {
     path: "/savingmbti",
     name: "savingmbti",
     component: () => import("../views/MbtiSave.vue"),
