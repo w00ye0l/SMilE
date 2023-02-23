66708cd23f0e329fb06c6be538b185a658a7ef08
hobin49 <hobin505@gmail.com>
2023-02-23 11:48:11 +0900

    fix : 네 번째 시작페이지 스마일 위치 변경 [이호빈]

--- src/views/Entrance/FourthPage.vue
+++ src/views/Entrance/FourthPage.vue
@@ -2,26 +2,29 @@
   <div class="background">
     <div class="head">
       <span class="header">SM</span>
-      <img :src="require(`@/assets/${imgName1}.png`)" class="title-img" />
+      <img :src="require(`@/assets/first_smile1.png`)" class="title-img" />
       <span class="header">E</span>
     </div>
     <div class="slider">
       <div class="main_content">
         <h3>
-          하루 하나의 질문을 통해<br />
+          하루의 하나의 질문을 통해<br />
           다른 <span class="mbti">MBTI</span>의 생각을 확인해요!
         </h3>
       </div>
       <div>
-        <img :src="require(`@/assets/first_smile8.png`)" class="left-img1" />
-        <img :src="require(`@/assets/first_question.png`)" class="question-mark" />
+        <img :src="require(`@/assets/first_smile8.png`)" class="left1-img" />
+        <img
+          :src="require(`@/assets/first_question.png`)"
+          class="question-mark"
+        />
         <img :src="require(`@/assets/exmark.png`)" class="ex-mark" />
-        <img :src="require(`@/assets/first_smile9.png`)" class="right-img1" />
-        <img :src="require(`@/assets/first_smile10.png`)" class="right-img2" />
-        <img :src="require(`@/assets/first_smile11.png`)" class="right-img3" />
-        <img :src="require(`@/assets/first_smile12.png`)" class="right-img4" />
-        <img :src="require(`@/assets/first_smile13.png`)" class="right-img5" />
-        <img :src="require(`@/assets/first_smile14.png`)" class="right-img6" />
+        <img :src="require(`@/assets/first_smile11.png`)" class="right-img1" />
+        <img :src="require(`@/assets/first_smile12.png`)" class="right-img2" />
+        <img :src="require(`@/assets/first_smile9.png`)" class="right-img3" />
+        <img :src="require(`@/assets/first_smile10.png`)" class="right-img4" />
+        <img :src="require(`@/assets/first_smile14.png`)" class="right-img5" />
+        <img :src="require(`@/assets/first_smile13.png`)" class="right-img6" />
       </div>
     </div>
     <button :disabled="isDisabled" class="btn btn-start">시작하기</button>
@@ -77,6 +80,63 @@ export default {
   color: #f59607;
 }
 
+.left1-img {
+  position: absolute;
+  width: 230px;
+  top: 350px;
+  left: -70px;
+}
+
+.question-mark {
+  position: absolute;
+  width: 70px;
+  top: 255px;
+  left: 35px;
+}
+
+.ex-mark {
+  position: absolute;
+  top: 230px;
+  width: 30px;
+  right: 30px;
+}
+
+.right-img1 {
+  position: absolute;
+  top: 375px;
+  right: 78px;
+}
+
+.right-img2 {
+  position: absolute;
+  top: 400px;
+  right: -20px;
+}
+
+.right-img3 {
+  position: absolute;
+  top: 275px;
+  right: 50px;
+}
+
+.right-img4 {
+  position: absolute;
+  top: 300px;
+  right: -45px;
+}
+
+.right-img5 {
+  position: absolute;
+  top: 495px;
+  right: -20px;
+}
+
+.right-img6 {
+  position: absolute;
+  top: 480px;
+  right: 80px;
+}
+
 .btn-start {
   width: 180px;
   height: 52px;
@@ -86,61 +146,5 @@ export default {
   font-size: 18px;
   font-weight: 700;
   border: none;
-}
-
-.left-img1 {
-  position: absolute;
-  width: 230px;
-  top: 300px;
-  left: -70px;
-}
-
-.question-mark {
-  position: absolute;
-  width: 70px;
-  top: 215px;
-  left: 35px;
-}
-
-.ex-mark {
-  position: absolute;
-  top: 190px;
-  width: 30px;
-  right: 30px;
-}
-
-.right-img1 {
-  position: absolute;
-  top: 335px;
-  right: 68px;
-}
-
-.right-img2 {
-  position: absolute;
-  top: 360px;
-  right: -30px;
-}
-
-.right-img3 {
-  position: absolute;
-  top: 235px;
-  right: 40px;
-}
-
-.right-img4 {
-  position: absolute;
-  top: 260px;
-  right: -55px;
-}
-
-.right-img5 {
-  position: absolute;
-  top: 455px;
-  right: -30px;
-}
-
-.right-img6 {
-  position: absolute;
-  top: 440px;
 }
 </style>
