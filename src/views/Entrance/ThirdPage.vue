c8e186f7774cc8b71caec04371caca5a5dcfddef
hobin49 <hobin505@gmail.com>
2023-02-23 11:02:12 +0900

    add : 세 번째 페이지 생성 [이호빈]

---
+++ src/views/Entrance/ThirdPage.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="background">
+    <div class="head">
+      <span class="header">SM</span>
+      <img :src="require(`@/assets/first_smile1.png`)" class="title-img" />
+      <span class="header">E</span>
+    </div>
+    <div class="slider">
+      <div class="main_content">
+        <h3>
+          하루 하나의 질문을 통해<br />
+          다른 <span class="mbti">MBTI</span>의 생각을 확인해요!
+        </h3>
+      </div>
+      <div>
+        <img :src="require(`@/assets/first_smile7.png`)" class="left-img" />
+        <img :src="require(`@/assets/first_info.png`)" class="right-info" />
+      </div>
+    </div>
+    <button :disabled="isDisabled" class="btn btn-start">시작하기</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    isDisabled() {
+      return true;
+    },
+  },
+};
+</script>
+<style scoped>
+.background {
+  position: relative;
+  height: 100vh;
+  background-color: #fff9c8;
+}
+
+.head {
+  padding-top: 30px;
+  margin: 0;
+}
+
+.header {
+  color: #f59607;
+  font-size: 2.5rem;
+  margin-top: 0.67rem 0 0.67rem 0;
+  font-weight: bold;
+}
+
+.title-img {
+  margin-top: 5px;
+  padding-left: 1px;
+  width: 30px;
+  height: 30px;
+}
+
+.slider {
+  height: 600px;
+}
+
+.main_content {
+  font-weight: bold;
+  font-size: 25px;
+}
+
+.mbti {
+  color: #f59607;
+}
+
+.left-img {
+  position: absolute;
+  left: 20px;
+  top: 295px;
+  width: 250px;
+}
+
+.right-info {
+  position: absolute;
+  top: 300px;
+  right: 35px;
+  width: 100px;
+}
+
+.btn-start {
+  width: 180px;
+  height: 52px;
+  border-radius: 25px;
+  color: #ffffff;
+  background-color: #c8c8c8;
+  font-size: 18px;
+  font-weight: 700;
+  border: none;
+}
+</style>
