f55eb74a12a56d62d29039bdda13012cbb6ba314
hobin49 <hobin505@gmail.com>
2023-02-24 11:28:48 +0900

    add : mbti 선택 파트 이미지 추가 [이호빈]

--- src/views/MbtiSave.vue
+++ src/views/MbtiSave.vue
@@ -1,6 +1,33 @@
 <template>
-  <h2>내 주변엔?</h2>
+  <div>
+    <h2 class="title">내 주변엔?</h2>
+    <div class="add-tab">
+      <h3 class="left-tab">그룹</h3>
+      <h3 class="right-tab">버튼</h3>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
 </script>
+<style scoped>
+.title {
+  margin: 0;
+  padding-top: 40px;
+}
+
+.add-tab {
+  display: flex;
+  justify-content: space-between;
+}
+
+.left-tab {
+  font-size: 1.4rem;
+  margin-left: 1.2rem;
+}
+
+.right-tab {
+  font-size: 1.4rem;
+  margin-right: 1.2rem;
+}
+</style>
