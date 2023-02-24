9f848726f167dbf69fd2a0640a13c056aaa22b88
hobin49 <hobin505@gmail.com>
2023-02-24 17:29:48 +0900

    add : 그룹 추가페이지 생성 [이호빈]

--- src/views/GroupAdd.vue
+++ src/views/GroupAdd.vue
@@ -1,16 +1,103 @@
 <template>
-  <div>
-    <h2>그룹 추가</h2>
-    <p class="category_name">내 그룹</p>
+  <div class="group-info">
+    <div>
+      <h2>그룹 추가</h2>
+      <p class="category-name1">내 그룹</p>
+      <div class="group-manage">
+        <span class="group-name">가족</span>
+        <div class="add-delete-img">
+          <img :src="require(`@/assets/pencil.png`)" class="pencil" />
+          <img :src="require(`@/assets/trashcan.png`)" class="trashcan" />
+        </div>
+      </div>
+    </div>
+    <div class="group-manage">
+      <span class="group-name">친구</span>
+      <div class="add-delete-img">
+        <img :src="require(`@/assets/pencil.png`)" class="pencil" />
+        <img :src="require(`@/assets/trashcan.png`)" class="trashcan" />
+      </div>
+    </div>
+  </div>
+  <div class="add-group">
+    <p class="category-name2">추가할 그룹 명</p>
+    <input
+      type="text"
+      placeholder="그룹 명"
+      v-model="groupId"
+      class="input-box"
+    />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      groupId: "",
+    };
+  },
+};
+</script>
+
 <style scoped>
-.category_name {
+.group-info {
+  height: 600px;
+}
+.category-name1 {
   display: flex;
   font-size: 20px;
   font-weight: 600;
   justify-content: left;
   margin-left: 20px;
+}
+
+.group-manage {
+  display: flex;
+  justify-content: space-between;
+  width: 90vw;
+  margin: 0 0 15px 20px;
+  border-radius: 20px;
+  background-color: #ffd338;
+  box-shadow: 0px 1.5px 0px 1.5px #d3d3d3;
+}
+
+.group-name {
+  margin: 10px 0 0 30px;
+  font-size: 20px;
+}
+
+.add-delete-img {
+  margin: 10px 10px 0 0;
+}
+
+.pencil {
+  margin: 0 10px 5px 0;
+}
+
+.trashcan {
+  margin: 0 0 5px 0;
+}
+.add-group {
+  background: #fff9c8;
+  height: 400px;
+}
+
+.category-name2 {
+  display: flex;
+  font-size: 20px;
+  font-weight: 600;
+  justify-content: left;
+  margin-left: 20px;
+  padding-top: 30px;
+}
+
+.input-box {
+  width: 90vw;
+  margin: 0 0 15px 0;
+  border-radius: 20px;
+  border: none;
+  box-shadow: 0px 1.5px 0px 1.5px #d3d3d3;
+  height: 45px;
 }
 </style>
