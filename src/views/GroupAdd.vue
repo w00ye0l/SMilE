2865174a2487bd2ce9676261cc4690d9fbc42603
hobin49 <hobin505@gmail.com>
2023-02-24 16:18:51 +0900

    add : 그룹추가 페이지 이미지 삽입 [이호빈]

---
+++ src/views/GroupAdd.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>그룹 추가</h2>
+    <p class="category_name">내 그룹</p>
+  </div>
+</template>
+
+<style scoped>
+.category_name {
+  display: flex;
+  font-size: 20px;
+  font-weight: 600;
+  justify-content: left;
+  margin-left: 20px;
+}
+</style>
