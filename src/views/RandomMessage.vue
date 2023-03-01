32adc81c11d66b69587f6272d1653b4d68737752
hobin49 <hobin505@gmail.com>
2023-03-01 23:44:37 +0900

    fix : 랜덤메시지 스타일 수정 [이호빈]

--- src/views/RandomMessage.vue
+++ src/views/RandomMessage.vue
@@ -12,6 +12,11 @@
         </option>
       </select>
     </div>
+    <div>
+      <img :src="require(`@/assets/randomimg1.png`)" class="img1" />
+      <img :src="require(`@/assets/randomimg3.png`)" class="img2" />
+      <img :src="require(`@/assets/randomimg2.png`)" class="img3" />
+    </div>
   </div>
 </template>
 <script>
@@ -51,6 +56,7 @@ export default {
 .background {
   background-color: #fff9c8;
   height: 100vh;
+  position: relative;
 }
 
 .select {
@@ -73,5 +79,26 @@ export default {
   height: 45px;
   display: inline-block;
   background-color: white;
+}
+
+.img1 {
+  position: absolute;
+  left: -55px;
+  top: 250px;
+  width: 230px;
+}
+
+.img2 {
+  position: absolute;
+  top: 250px;
+  right: -40px;
+  width: 230px;
+}
+
+.img3 {
+  position: absolute;
+  top: 220px;
+  right: 130px;
+  width: 160px;
 }
 </style>
