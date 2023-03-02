9d05fdaf2440d4358163378ca06dbaa0d22f9193
hobin49 <hobin505@gmail.com>
2023-03-02 11:49:40 +0900

    add : 취소 버튼 이미지 추가 [이호빈]

--- src/views/SendMessage.vue
+++ src/views/SendMessage.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="background">
-    <h2>1</h2>
+  <div class="title background">
+    <h2>쪽지 보내기</h2>
+    <div class="messagebox">
+      <div class="close-send">
+        <span>x</span>
+        <button class="send-btn">보내기</button>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped>
+.title {
+  margin: 0;
+  padding: 30px 0 20px 0;
+}
 .background {
   background-color: #fff9c8;
   height: 100vh;
+}
+
+.messagebox {
+  height: 70vh;
+  width: 70vw;
+  margin: 40px 0 0 60px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0px 1.5px 0px 1.5px #d3d3d3;
+}
+
+.close-send {
+  display: flex;
+  justify-content: space-between;
+}
+
+.send-btn {
+  height: 20px;
 }
 </style>
