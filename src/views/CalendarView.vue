86ab2383dbed2d280ca6254853359b877e0ab0f1
wooyeol <dnduf158@gmail.com>
2023-03-02 14:26:33 +0900

    add : 캘린더 페이지 추가 [이우열]

---
+++ src/views/CalendarView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-calendar />
+    <!-- <v-date-picker v-model="date" /> -->
+  </div>
+</template>
+
+<script>
+import "v-calendar/dist/style.css";
+
+export default {};
+</script>
+
+<style></style>
