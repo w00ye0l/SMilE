0103edba8ade3f76219600d98bd3a7ec5a9c569a
hobin49 <hobin505@gmail.com>
2023-03-05 22:58:29 +0900

    fix : message 수정 [이호빈]

--- src/views/RandomAnswer.vue
+++ src/views/RandomAnswer.vue
@@ -4,18 +4,14 @@
     <div class="question">
       <span class="letter">{{ message }}</span>
     </div>
-    <div
-      class="memo-box"
-      v-for="(idx, value, name) in messageList"
-      v-bind:key="name"
-    >
+    <div class="memo-box" v-for="(item, index) in messageList" :key="index">
       <div class="img-title">
         <img :src="require(`@/assets/first_smile1.png`)" class="title-img" />
         <span class="mbti"> {{ mbti }}</span>
       </div>
       <br />
       <div class="answer">
-        {{ idx }}
+        {{ item.name }}
       </div>
     </div>
   </div>
@@ -28,7 +24,12 @@ export default {
       message: "Q.친구가 기분이 안좋아서 화분을 샀다고 했다. 이때 나의 대답은?",
       answer: "무슨 일이야!",
       mbti: "ENFP",
-      messageList: { name: "기분", day: "날씨" },
+      messageList: [
+        { name: "엥 기분이 왜  안 좋았지?" },
+        { name: "무슨 일이야" },
+        { name: "나쁜일 있어?" },
+        { name: "기분이 별로네" },
+      ],
     };
   },
 };
@@ -37,6 +38,7 @@ export default {
 .title {
   margin: 0;
   padding: 30px 0 20px 0;
+  overflow: scroll;
 }
 .background {
   background-color: #fff9c8;
