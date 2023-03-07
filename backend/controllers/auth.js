e166bb81128a082fd16ae5f462dffa9524f133bf
rhoeunbin <rhoeb21@gmail.com>
2023-03-07 16:19:57 +0900

    add : 로그인 기능 추가 [노은빈]

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,8 +6,16 @@ const db = mysql.createConnection({ // 모듈과 관련된 객체를 넣음
   host: process.env.DB_HOST, // db서버가 어떤 곳에 있느냐
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB__NAME,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+});
+
+db.connect( (error) => {
+  if(error) {
+    console.log(error);
+  } else {
+    console.log("MySQL Connected, controllers");
+  }
 });
 
 // 회원가입
@@ -21,30 +29,56 @@ exports.signup = (req, res) => {
       if (error) {
         console.log(error);
       };
-      if (results.length > 0) {
-        return res.render('signup', {
-          message: 'that email is already in use'
-        });
-      } else if (password !== passwordConfirm) {
-        return res.render('signup', {
-          message: 'password do not match'
-        });
-      };
+      
   
       let hashedPassword = await bcrypt.hash(password, 8);
-      console.log(hashedPassword);
+      // console.log(hashedPassword);
   
       db.query('INSERT INTO user SET ?', { nickname: nickname, email: email, password: hashedPassword, gender: gender, birthday: birthday, mbti1: mbti1, mbti2: mbti2, mbti3: mbti3, mbti4: mbti4, image: image}, (error, results) => {
         if (error) {
           console.log(error);
         } else {
           console.log(results);
-          return res.render('signup', {
-            message: 'user signup'
-          });
         };
       });
     });
   res.send("form submitted");
 };
 
+// 로그인
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if ( !email || !password ){
+      console.log('nothing')
+    }
+
+    db.query('SELECT * FROM user WHERE email = ?', [email], async (error, results) => {
+      console.log(results);
+      if( !results || !(await bcrypt.compare(password, results[0].password )) ) {
+        res.status(401)
+      } else {
+        const id = results[0].id; 
+
+        const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRES_IN
+        });
+
+        console.log("the token is:" + token)
+
+        const cookieOptions = {
+          expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+          ),
+          httpOnly: true
+        }
+
+        res.cookie('jwt', token, cookieOptions );
+        res.status(200).redirect("/");
+      }
+    })
+  } catch (error) {
+    console.log(error);
+  }
+};
