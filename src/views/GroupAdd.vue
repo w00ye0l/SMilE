b4e9a4d23acb09bcd47b5b878060c97be80ba91a
hobin49 <hobin505@gmail.com>
2023-03-13 14:01:00 +0900

    fix : 시작페이지-마지막 슬라이드 버튼 활성화 [이호빈]

--- src/views/GroupAdd.vue
+++ src/views/GroupAdd.vue
@@ -2,35 +2,41 @@
   <div class="group-info">
     <div>
       <h2>그룹 추가</h2>
-      <p class="category-name1">내 그룹</p>
-      <div class="group-manage">
-        <span class="group-name">가족</span>
-        <div class="add-delete-img">
-          <img :src="require(`@/assets/pencil.png`)" class="pencil" />
-          <img :src="require(`@/assets/trashcan.png`)" class="trashcan" />
+      <div class="category-addbtn">
+        <p class="category-name1">내 그룹</p>
+        <div class="add-control">
+          <button class="group-add-btn" @click="addBtn()">추가</button>
         </div>
       </div>
-    </div>
-    <div class="group-manage">
-      <span class="group-name">친구</span>
-      <div class="add-delete-img">
-        <img :src="require(`@/assets/pencil.png`)" class="pencil" />
-        <img :src="require(`@/assets/trashcan.png`)" class="trashcan" />
+      <div class="group-manage">
+        <div class="group-name" v-for="(item, value) in groups" :key="value">
+          <span class="name">{{ item }}</span>
+          <div>
+            <div class="add-delete-img">
+              <button class="fix-btn" @click="fixBtn()">
+                <img :src="require(`@/assets/pencil.png`)" class="pencil" />
+              </button>
+              <button class="delete-btn">
+                <img :src="require(`@/assets/trashcan.png`)" class="trashcan" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
   <div class="add-group">
-    <p class="category-name2">추가할 그룹 명</p>
+    <p class="category-name2">{{ category }}</p>
     <input
       type="text"
-      placeholder="그룹 명"
+      :placeholder="placeholder"
       v-model="groupId"
       class="input-box"
     />
     <br />
     <div class="btn-control">
-      <button class="add-btn" @click="pageLink">추가</button>
-      <button class="cancel-btn" @click="pageLink">취소</button>
+      <button class="add-btn" @click="registerBtn()">{{ button }}</button>
+      <button class="cancel-btn" @click="pageLink()">취소</button>
     </div>
   </div>
 </template>
@@ -40,11 +46,36 @@ export default {
   data() {
     return {
       groupId: "",
+      placeholder: "",
+      content: "-",
+      category: "-",
+      button: "-",
+      members: "",
+      groups: {},
+      key: 0,
     };
   },
   methods: {
     pageLink() {
       this.$router.push({ path: "savingmbti" });
+    },
+    addBtn() {
+      this.content = this.groupId;
+      this.category = "추가할 그룹 명";
+      this.button = "추가";
+      this.placeholder = "그룹 명";
+    },
+    fixBtn() {
+      this.placeholder = this.groupId;
+      this.content = this.groupId;
+      this.category = "그룹 및 수정";
+      this.button = "수정";
+      // this.groups[this.key] = this.content;
+    },
+    registerBtn() {
+      this.content = this.groupId;
+      this.groups[this.key] = this.content;
+      console.log(this.groups);
     },
   },
 };
@@ -54,17 +85,35 @@ export default {
 .group-info {
   height: 600px;
 }
+
+.category-addbtn {
+  display: flex;
+  justify-content: space-between;
+}
+
+.add-control {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.group-add-btn {
+  margin-right: 20px;
+  width: 70px;
+  height: 40px;
+  border-radius: 17px;
+  border: none;
+  background-color: #f59607;
+  font-size: 17px;
+}
 .category-name1 {
   display: flex;
   font-size: 20px;
   font-weight: 600;
   justify-content: left;
-  margin-left: 20px;
+  margin-left: 25px;
 }
 
 .group-manage {
-  display: flex;
-  justify-content: space-between;
   width: 90vw;
   margin: 0 0 15px 20px;
   border-radius: 20px;
@@ -73,8 +122,10 @@ export default {
 }
 
 .group-name {
-  margin: 10px 0 0 30px;
+  margin: 10px 0 30px 30px;
   font-size: 20px;
+  display: flex;
+  justify-content: space-between;
 }
 
 .add-delete-img {
@@ -82,7 +133,7 @@ export default {
 }
 
 .pencil {
-  margin: 0 10px 5px 0;
+  margin: 0 0 5px 0;
 }
 
 .trashcan {
@@ -140,5 +191,20 @@ input::placeholder {
   background-color: #ffffff;
   margin-right: 50px;
   font-size: 17px;
+}
+
+.fix-btn {
+  border: none;
+  background-color: transparent;
+}
+.delete-btn {
+  border: none;
+  background-color: transparent;
+}
+
+.name {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
