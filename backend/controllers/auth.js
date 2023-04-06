770a8fcbdce014ade47628f760a7777af0fab4ef
rhoeunbin <rhoeb21@gmail.com>
2023-04-06 19:44:04 +0900

    fix : 회원가입 기능 수정 [노은빈]

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,95 +1,59 @@
+const express = require('express');
 const mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const { promisify } = require('util');
 
-// Database
-const db = mysql.createConnection({ // 모듈과 관련된 객체를 넣음
-  host: process.env.DB_HOST, // db서버가 어떤 곳에 있느냐
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+// // Database
+// const db = mysql.createConnection({ // 모듈과 관련된 객체를 넣음
+//   host: process.env.DB_HOST, // db서버가 어떤 곳에 있느냐
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
 
-db.connect( (error) => {
-  if(error) {
-    console.log(error);
-  } else {
-    console.log("MySQL Connected, controllers");
-  }
-});
+// db.connect( (error) => {
+//   if(error) {
+//     console.log(error);
+//   } else {
+//     console.log("MySQL Connected, controllers");
+//   }
+// });
+
+const User = require('../models/user');
+
+const router = express.Router();
 
 // 회원가입
-exports.signup = (req, res) => {
-  
-  const { nickname, email, password, passwordConfirm, birthday, gender, mbti1, mbti2, mbti3, 
-    mbti4, image } = req.body;   
-  console.log(req.body);
-
-    db.query('SELECT email FROM user WHERE email = ?', [email], async (error, results) => {
-      if (error) {
-        console.log(error);
-      };
-      
-  
-      let hashedPassword = await bcrypt.hash(password, 8);
-      // console.log(hashedPassword);
-  
-      db.query('INSERT INTO user SET ?', { nickname: nickname, email: email, password: hashedPassword, gender: gender, birthday: birthday, mbti1: mbti1, mbti2: mbti2, mbti3: mbti3, mbti4: mbti4, image: image}, (error, results) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(results);
-        };
-      });
-    });
-  res.send("form submitted");
-};
-
-// 로그인
-exports.login = async (req, res) => {
+exports.signup = async (req, res, next) => {
+  const { nickname, email, password, birthday, gender, mbti1, mbti2, mbti3, mbti4, image } = req.body;
   try {
-    const { email, password } = req.body;
-
-    if ( !email || !password ){
-      console.log('nothing')
+    // 이메일 중복 가입 방지
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      console.log('signuppp')
+      return next();
     }
+    // 정상적인 회원가입 절차면 해시화
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-    db.query('SELECT * FROM user WHERE email = ?', [email], async (error, results) => {
-      console.log(results);
-      if( !results || !(await bcrypt.compare(password, results[0].password )) ) {
-        res.status(401)
-        console.log('not match')
-      } else {
-        const id = results[0].id; 
-        const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-          expiresIn: process.env.JWT_EXPIRES_IN
-        });
-
-        console.log("the token is:" + token);
-
-        const cookieOptions = {
-          expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-          ),
-          httpOnly: true,
-          // sameSite: 'none',
-          secure: true,
-          doamin: 'localhost',
-          credentials: true
-        }
-
-        res.cookie('jwt', token, cookieOptions );
-        // res.send('cookie')
-        // res.send(jwt)
-        // res.status(200).redirect("/mypage");
-        res.status(200)
-        console.log('login success');
-        res.send("login!!!!!")
-      }
-    })
+    // DB에 해당 회원정보 생성
+    await User.create({ 
+      nickname: nickname, 
+      email: email, 
+      password: hashedPassword, 
+      gender: gender, 
+      birthday: birthday, 
+      mbti1: mbti1, mbti2: mbti2, mbti3: mbti3, mbti4: mbti4, 
+      image: image,
+    });
+    console.log('11');
+    res.end()
+    // return res.redirect('/login');
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return next(error);
   }
 };
--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -3,16 +3,19 @@ const authController = require('../controllers/auth.js');
 
 const router = express.Router();
 
+// 메인페이지
 router.get('/', (req, res) => {
   console.log('home');
 });
 
-router.get('/signup', (req, res) => {
-  console.log('signup');
-});
-
-router.get('/login', (req, res) => {
-  console.log('login');
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  // res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  // res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  // res.locals.followerIdList = req.user
+  //   ? req.user.Followings.map((f) => f.id)
+  //   : [];
+  next();
 });
 
 module.exports = router;
