0524199935aed599cc0fe752c0bd388a7c66ce12
rhoeunbin <rhoeb21@gmail.com>
2023-04-06 19:54:00 +0900

    fix : 로그인 기능 수정 [노은빈]

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -57,3 +57,27 @@ exports.signup = async (req, res, next) => {
     return next(error);
   }
 };
+
+exports.login = async (req, res, next) => { 
+  //? local로 실행이 되면 localstrategy.js를 찾아 실행
+  passport.authenticate('local', {session: false}, (authError, user, info) => {
+    // done(err)가 처리된 경우
+    if (authError) {
+      console.error(authError);
+      return next(authError); // 에러처리 미들웨어로 보낸다.
+    }
+    // done(null, false, { message: '비밀번호가 일치하지 않습니다.' }) 가 처리된 경우
+    if (!user) {
+      return res.redirect(`/?loginError=${info.message}`);
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      console.log('login success22');
+      res.end();
+    });
+
+  })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
+};
