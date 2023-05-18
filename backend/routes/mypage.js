c947bf6e47e3433e1d22f98f84fe0745e354ab79
rhoeunbin <rhoeb21@gmail.com>
2023-05-18 15:04:37 +0900

    add : 비밀번호 변경 기능 구현 [노은빈]

--- backend/routes/mypage.js
+++ backend/routes/mypage.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
-const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { myprofile } = require('../controllers/mypage');
+const { isLoggedIn } = require('../middlewares');
+const { myprofile, changePW } = require('../controllers/mypage');
 
 const router = express.Router();
 
 // GET /mypage
 router.get('/', isLoggedIn, myprofile);
+
+// PUT /mypage/edit
+router.put('/edit', isLoggedIn, changePW);
 
 module.exports = router;
