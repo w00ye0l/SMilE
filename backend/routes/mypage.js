cc55275d89e1d9bf79d144ec05142ef19c1d21f0
rhoeunbin <rhoeb21@gmail.com>
2023-08-31 00:36:48 +0900

    fix : 로그인 테스트 [노은빈]

--- backend/routes/mypage.js
+++ backend/routes/mypage.js
@@ -6,7 +6,7 @@ const { myProfile, updateProfile, editPW } = require('../controllers/mypage');
 const router = express.Router();
 
 // GET /mypage
-router.get('/', myProfile);
+router.get('/', isLoggedIn, myProfile);
 
 // PUT /mypage/update
 router.put('/update', isLoggedIn, updateProfile);
