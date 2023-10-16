5497ba96ffd39d44c1842adab4bee7a3ef0b4789
rhoeunbin <rhoeb21@gmail.com>
2023-10-17 01:44:36 +0900

    fix : passport deserialize 수정 [노은빈]

--- backend/passport/index.js
+++ backend/passport/index.js
@@ -8,11 +8,17 @@ module.exports = () => {
     done(null, user.id); 
   });
 
-  passport.deserializeUser((req, id, done) => {
-    console.log('deserializerUser', req.session);
+  passport.deserializeUser((id, done) => {
+    console.log('deserializeUser', req.session);
     User.findOne({ where: { id } })
-      .then(user => done(null, user))
+      .then(user => {
+        if (!user) {
+          return done(null, false);
+        }
+        done(null, user); 
+      })
       .catch(err => done(err));
   });
+
   local();
 };
