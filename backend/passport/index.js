a3427ac83c4b545de8ab435f80207b161d9b0c55
rhoeunbin <rhoeb21@gmail.com>
2023-10-20 17:39:59 +0900

    fix : passport 미들웨어 로그 수정 [노은빈]

--- backend/passport/index.js
+++ backend/passport/index.js
@@ -4,21 +4,27 @@ const User = require('../models/user');
 
 module.exports = () => {
   passport.serializeUser((req, user, done) => {
-    console.log('serializeUser.id', user.id);
+    console.log('serializeUserID', user.id);
     console.log('serializeSession', req.session);
     done(null, user.id); 
   });
 
   passport.deserializeUser((req, id, done) => {
+    console.log('deserializeUserID', id);
     console.log('deserializeUser', req.session);
     User.findOne({ where: { id } })
       .then(user => {
         if (!user) {
+          console.log('User 없음');
           return done(null, false);
         }
+        console.log('User 있음', user);
         done(null, user); 
       })
-      .catch(err => done(err));
+      .catch(err => {
+        console.error('deserializeError:', err);
+        done(err);
+      });
   });
   
   local();
