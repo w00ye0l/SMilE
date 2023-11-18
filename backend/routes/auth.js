6d8aa58e563a48eb8ed2802e2d373d6b8e1a1d28
rhoeunbin <rhoeb21@gmail.com>
2023-11-18 22:28:10 +0900

    fix : 이미지 업로드 라우터 수정 [노은빈]

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,21 +1,31 @@
-const express = require('express');
-const { upload } = require('../middlewares/upload');
+const express = require("express");
+const { upload } = require("../middlewares/upload");
 
-const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { signup, login, logout, remove } = require('../controllers/auth');
+const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
+const { signup, login, logout, remove } = require("../controllers/auth");
 
 const router = express.Router();
 
+// 이미지 업로드 테스트용 라우터
+// POST / auth / image
+router.post("/image", upload.single("image"), (req, res) => {
+  // console.log(req);
+  console.log(req.file);
+  res.json({
+    url: req.file.location,
+  });
+});
+
 // POST /auth/signup
-router.post('/signup', upload.single('image'), signup);
+router.post("/signup", isNotLoggedIn, upload.single("image"), signup);
 
 // POST /auth/login
-router.post('/login', isNotLoggedIn, login);
+router.post("/login", isNotLoggedIn, login);
 
 // GET /auth/logout
-router.get('/logout', isLoggedIn, logout);
+router.get("/logout", isLoggedIn, logout);
 
 // DELETE /auth/delete
-router.delete('/remove/:id', isLoggedIn, remove)
+router.delete("/remove/:id", isLoggedIn, remove);
 
 module.exports = router;
