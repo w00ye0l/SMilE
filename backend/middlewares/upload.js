7ba62256df5a761c431796128fa81dfcd0e520b8
rhoeunbin <rhoeb21@gmail.com>
2023-11-18 22:27:49 +0900

    fix : 이미지 업로드 미들웨어 수정 [노은빈]

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -1,28 +1,52 @@
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-const aws = require('aws-sdk');
-require('dotenv').config();
+const multer = require("multer");
+const path = require("path");
+const AWS = require("aws-sdk");
+const multerS3 = require("multer-s3");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEYID, // keyID 입력 
-  secretAccessKey: process.env.S3_PRIVATE_KEY, // 시크릿 키 입력
-  region: process.env.REGION, // 버킷 생성 리전 입력
+// upload 테스트용 코드
+// const upload = multer({
+//   dest: "uploads/", // 테스트를 위한 로컬 폴더 사용
+//   fileFilter: (req, file, cb) => {
+//     if (file.mimetype.startsWith("image/")) {
+//       cb(null, true);
+//     } else {
+//       cb(new Error("잘못된 파일 유형입니다."));
+//     }
+//   },
+//   limits: { fileSize: 5 * 1024 * 1024 },
+// });
+
+// aws region 및 자격증명 설정
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
 });
 
 const upload = multer({
   storage: multerS3({
-    s3: s3,
-    bucket: process.env.BUCKET_NAME, // 버킷 이름
+    s3: new AWS.S3(),
+    bucket: "smile-img", // 버킷 이름
     contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: 'public-read-write',
+    acl: "public-read-write",
     key: (req, file, cb) => {
-      file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
-      console.log("file", file);
-      cb(null, `profileimg/${Date.now()}_${file.originalname}`);
+      file.originalname = Buffer.from(file.originalname, "latin1").toString(
+        "utf8"
+      );
+      cb(null, `profileimg/${Date.now()}_${path.basename(file.originalname)}`);
     },
   }),
-  // 용량 제한
+  // 오류 처리
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image")) {
+      cb(null, true);
+    } else {
+      cb(new Error("잘못된 파일 유형입니다."));
+    }
+  },
   limits: { fileSize: 5 * 1024 * 1024 },
 });
 
-exports.upload = multer(upload);
+exports.upload = upload;
