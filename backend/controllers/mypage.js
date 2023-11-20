f1cb2feb1d614531bae12e0fa6eaa63df6a0b157
rhoeunbin <rhoeb21@gmail.com>
2023-11-21 01:01:35 +0900

    add : 프로필 이미지 수정 및 삭제 코드 추가 [노은빈]

--- backend/controllers/mypage.js
+++ backend/controllers/mypage.js
@@ -1,27 +1,26 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const AWS = require("aws-sdk");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 // 마이페이지
-exports.myProfile = async (req, res, next) => { 
-  console.log(req.body);
-  console.log(req.user.id);
-  try{  
+exports.myProfile = async (req, res, next) => {
+  try {
     const user = await User.findOne({
       where: { id: req.user.id }, // 현재 로그인한 사용자의 ID를 조회
-      attributes: { exclude: ['password'] } // 비밀번호를 제외한 다른 속성만 선택
+      attributes: { exclude: ["password"] }, // 비밀번호를 제외한 다른 속성만 선택
     });
 
     res.send(user);
-    } catch(err){
-      console.error(err);
-      console.log("데이터 조회 실패");
-      next(err);
-    }
-  res.end();    
+  } catch (err) {
+    console.error(err);
+    console.log("데이터 조회 실패");
+    next(err);
+  }
+  res.end();
 };
 
-  //모든 사용자 조회
+//모든 사용자 조회
 //   try {
 //     const users = await User.findOne();
 //     console.log(req.user);
@@ -36,14 +35,13 @@ exports.myProfile = async (req, res, next) => {
 
 // 회원정보 수정
 exports.updateProfile = async (req, res, next) => {
-  const { nickname, email, birthday, gender, mbti1, mbti2, mbti3, mbti4, image } = req.body;
+  const { nickname, birthday, gender, mbti1, mbti2, mbti3, mbti4 } = req.body;
   try {
     // 현재 로그인한 사용자의 정보 가져오기
     const user = await User.findOne({ where: { id: req.user.id } });
     if (!user) {
-      return res.status(404).json({ message: '로그인한 사용자가 아닙니다!' });
+      return res.status(404).json({ message: "로그인한 사용자가 아닙니다!" });
     }
-
     // 회원 정보 업데이트
     user.nickname = nickname;
     user.birthday = birthday;
@@ -52,11 +50,26 @@ exports.updateProfile = async (req, res, next) => {
     user.mbti2 = mbti2;
     user.mbti3 = mbti3;
     user.mbti4 = mbti4;
-    user.image = image;
 
     await user.save();
-
     res.send(user);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
+// 이미지 삭제
+exports.deleteImage = async (req, res, next) => {
+  try {
+    const s3 = new AWS.S3();
+    const userId = req.user.id;
+
+    s3.deleteObject({
+      Bucket: "smile-img",
+      Key: `profileimg/${userId}`,
+    });
+    res.status(200).json({ message: "이미지 파일이 삭제되었습니다!" });
   } catch (error) {
     console.error(error);
     return next(error);
@@ -72,25 +85,30 @@ exports.editPW = async (req, res, next) => {
     const user = await User.findOne({ where: { id: req.user.id } });
 
     if (!user) {
-      return res.status(404).json({ message: '로그인한 사용자가 아닙니다!' });
+      return res.status(404).json({ message: "로그인한 사용자가 아닙니다!" });
     }
 
     // 현재 비밀번호 확인
     const isMatch = await bcrypt.compare(currentPassword, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+      return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
     }
 
     // 새로운 비밀번호 확인
     if (newPassword !== confirmPassword) {
-      return res.status(400).json({ message: '새 비밀번호가 일치하지 않습니다.' });
+      return res
+        .status(400)
+        .json({ message: "새 비밀번호가 일치하지 않습니다." });
     }
 
     // 새로운 비밀번호 해시화
     const hashedPassword = await bcrypt.hash(newPassword, 12);
 
     // 비밀번호 업데이트
-    await User.update({ password: hashedPassword }, { where: { id: req.user.id } });
+    await User.update(
+      { password: hashedPassword },
+      { where: { id: req.user.id } }
+    );
 
     res.end();
   } catch (error) {
--- backend/routes/mypage.js
+++ backend/routes/mypage.js
@@ -1,20 +1,34 @@
-const express = require('express');
+const express = require("express");
 
-const { isLoggedIn } = require('../middlewares');
-const { myProfile, updateProfile, editPW } = require('../controllers/mypage');
+const { upload } = require("../middlewares/upload");
+const { isLoggedIn } = require("../middlewares");
+const {
+  myProfile,
+  deleteImage,
+  updateProfile,
+  editPW,
+} = require("../controllers/mypage");
 
 const router = express.Router();
 
 // GET /mypage
-router.get('/', isLoggedIn, myProfile);
+router.get("/", isLoggedIn, myProfile);
 
-// POST /mypage
-router.post('/', isLoggedIn, myProfile);
+// POST /mypage/image
+router.post("/image", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  res.json({
+    url: req.file.location,
+  });
+});
+
+// DELETE /mypage/image/:userId
+router.delete("/image/:id", deleteImage);
 
 // PUT /mypage/update
-router.put('/update', isLoggedIn, updateProfile);
+router.put("/update", isLoggedIn, upload.single("image"), updateProfile);
 
 // PUT /mypage/editpw
-router.put('/editpw', isLoggedIn, editPW);
+router.put("/editpw", isLoggedIn, editPW);
 
 module.exports = router;
