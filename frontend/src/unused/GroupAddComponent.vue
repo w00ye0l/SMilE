e21c9f00cfb931b9da74a5f67e83ed82c8b70e2a
wooyeol <dnduf158@gmail.com>
2024-01-09 17:16:57 +0900

    del : 필요 없는 vue 파일 삭제 [이우열]

---
+++ frontend/src/unused/GroupAddComponent.vue
@@ -0,0 +1,68 @@
+<!-- <template></template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    category: String,
+    placeholder: String,
+    button: String,
+    propGroupName: String,
+    propGroupId: Number,
+    updateGroups: Function,
+  },
+  data() {
+    return {
+      groupName: this.propGroupName,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.group-form {
+  background: #fff9c8;
+  width: 100%;
+  padding: 30px;
+}
+
+.group-label {
+  margin-bottom: 10px;
+  display: flex;
+  font-size: 20px;
+  font-weight: 600;
+}
+
+.group-input {
+  margin-bottom: 30px;
+  padding: 10px;
+  width: 100%;
+  font-size: 16px;
+  border-radius: 20px;
+  border: 0;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25);
+}
+
+.btn-container {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.btn {
+  width: 100px;
+  height: 40px;
+  border-radius: 20px;
+  border: 0;
+  font-size: 20px;
+}
+
+.add-btn {
+  background-color: #f59607;
+}
+
+.cancel-btn {
+  background-color: #ffffff;
+}
+</style> -->
