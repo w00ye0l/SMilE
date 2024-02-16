4f9d45519f23e16d8d3ef31b1019585fbb00b489
wooyeol <dnduf158@gmail.com>
2024-02-16 18:28:36 +0900

    fix: 랜덤 질문 이동 버튼 조건 수정 [이우열]

--- frontend/src/views/user/MyPage.vue
+++ frontend/src/views/user/MyPage.vue
@@ -62,7 +62,7 @@
     </div>
 
     <!-- 오늘의 질문 컨테이너 -->
-    <div v-if="answered !== 0" class="question-container">
+    <div v-if="answered !== 0 && answered !== null" class="question-container">
       <h3 class="question-title">오늘의 질문</h3>
       <p class="question-subtitle">작성한 답변 확인하기</p>
 
